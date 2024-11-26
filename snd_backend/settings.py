e07b7dead86f72cca0f995634271a52bfc501d9a
moh-ashique <mohammedashique942001@gmail.com>
2024-11-26 09:28:26 +0530

    refresh token is completed

--- snd_backend/settings.py
+++ snd_backend/settings.py
@@ -202,3 +202,13 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+# settings.py
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+}
